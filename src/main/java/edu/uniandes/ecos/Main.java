1b20d363d3928dd0ac133d6487b4a8714df2d0e1
Sergio <sergioforerop@hotmail.com>
2015-02-02 22:38:39 -0500

    se realizaron los metodos de busqueda

--- src/main/java/edu/uniandes/ecos/Main.java
+++ src/main/java/edu/uniandes/ecos/Main.java
@@ -18,13 +18,13 @@ import org.eclipse.jetty.servlet.ServletHolder;
 public class Main extends HttpServlet {
 
     public static void main(String[] args) throws Exception {
-//        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-//        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-//        context.setContextPath("/");
-//        server.setHandler(context);
-//        context.addServlet(new ServletHolder(new Main()), "/*");
-//        server.start();
-//        server.join();
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/*");
+        server.start();
+        server.join();
         
         //---
         ContadorClases.buscarArchivo();
